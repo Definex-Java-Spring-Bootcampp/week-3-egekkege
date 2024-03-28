12b36e3298d5364e922687d2a911f95bb9b047ac
egekkege <kale.egemen@gmail.com>
2024-03-28 22:45:17 +0300

    initialized project

--- kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/configuration/BeanConfiguration.java
+++ kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/configuration/BeanConfiguration.java
@@ -2,6 +2,7 @@ package com.patika.kredinbizdeservice.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.ServerCodecConfigurer;
 
 @Configuration
 public class BeanConfiguration {
@@ -10,4 +11,9 @@ public class BeanConfiguration {
     public IUserService userService4() {
         return new UserService2();
     }*/
+
+    @Bean
+    public ServerCodecConfigurer serverCodecConfigurer() {
+        return ServerCodecConfigurer.create();
+    }
 }
